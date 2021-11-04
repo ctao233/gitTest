f26fd104954a8e5489fde53fba9b2e79194438b8
atao <ct1633516187@163.com>
2021-11-04 14:34:44 +0800

    ssh 推送 commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,5 +6,7 @@ public class Test
         System.out.println("第二次提交本地库");
         System.out.println("热修分支");
         System.out.println("ssh 免密登录");
+        System.out.println("pushi 推送 免密登录");
+
     }
 }
